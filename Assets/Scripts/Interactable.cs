47dc4688d068d225905831582017a83251a41267
kghobadi <kasghobadi@gmail.com>
2023-10-12 23:05:21 -0400

    got the basic code setup working - now i just need to ensure the animation setup is good.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Interactable : MonoBehaviour 
+{
+    protected GameObject player;
+    protected GameObject cursorObj;
+    protected Image cursorImg;
+    protected Sprite originalCursor;
+    public Sprite interactCursor;
+    public float withinClickDist = 10;
+
+    protected bool changedSprites;
+
+    void Awake () 
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        cursorObj = GameObject.FindGameObjectWithTag("Cursor");
+        cursorImg = cursorObj.GetComponent<Image>();
+        originalCursor = cursorImg.sprite;
+        changedSprites = false;
+    }
+
+    protected virtual void OnMouseOver()
+    {
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+        if (dist < withinClickDist && !changedSprites)
+        {
+            cursorImg.sprite = interactCursor;
+            changedSprites = true;
+        }
+        Debug.Log("on mouse over");
+    }
+
+    protected virtual void OnMouseDown()
+    {
+        if (changedSprites)
+        {
+            Interact();
+        }
+    }
+
+    protected virtual void OnMouseExit()
+    {
+        changedSprites = false;
+        cursorImg.sprite = originalCursor;
+    }
+
+    public virtual void Interact()
+    {
+        //do whatever the hell you want
+    }
+}
---
+++ Assets/Scripts/PetAnimal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+/// <summary>
+/// Pets an animal!
+/// </summary>
+public class PetAnimal : Interactable
+{
+    private AnimalSounds animalAudio;
+    private Animator p_Animator;
+
+    public string pettedTrigger = "petted";
+    private int randomIdle;
+    [SerializeField] private int idleMax;
+ 
+    private void Start()
+    {
+        animalAudio = GetComponent<AnimalSounds>();
+        p_Animator = GetComponentInParent<Animator>();
+
+        //set random idle anim 
+        randomIdle = Random.Range(0, idleMax);
+        p_Animator.SetFloat("idleType", randomIdle);
+    }
+
+    /// <summary>
+    /// Play animal sound. 
+    /// </summary>
+    public override void Interact()
+    {
+        base.Interact();
+
+        //play sound!
+        animalAudio.PlayRandomSoundRandomPitch(animalAudio.animalSounds, 1f);
+        p_Animator.SetTrigger(pettedTrigger); 
+    }
+}
