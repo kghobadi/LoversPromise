5f63c63e991c4638db43f77ffeb29fc6c3806fdd
kghobadi <kasghobadi@gmail.com>
2024-05-22 00:14:28 -0400

    Ethan's teleport is setup :)

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A simple teleportation class with an effect.
+/// </summary>
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private bool useFx = true;
+    [SerializeField] private ParticleSystem teleportEffect;
+    private ParticleSystem.MainModule teleportMain;
+
+    private void Awake()
+    {
+        teleportMain = teleportEffect.main;
+    }
+
+    public void TeleportToTransform(Transform newPlace) => TeleportToPosition(newPlace.position);
+    
+    public void TeleportToPosition(Vector3 newPosition)
+    {
+        if (useFx)
+        {
+            StartCoroutine(TeleportEffect(newPosition));
+        }
+        else
+        {
+            transform.position = newPosition;
+        }
+    }
+
+    /// <summary>
+    /// Teleports with effect.
+    /// </summary>
+    /// <param name="spot"></param>
+    /// <returns></returns>
+    IEnumerator TeleportEffect(Vector3 spot)
+    {
+        teleportEffect.Play();
+
+        yield return new WaitForSeconds(teleportMain.duration);
+
+        transform.position = spot;
+        
+        teleportEffect.Play();
+    }
+}
